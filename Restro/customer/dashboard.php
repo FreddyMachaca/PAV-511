ef60fde73b78d4be7a4412b51e6d632c1921dcc2
TFreddy <freddyymachaca@gmail.com>
2023-06-21 23:37:30 -0400

    solucion de bug en la creacion de cuenta

--- Restro/customer/dashboard.php
+++ Restro/customer/dashboard.php
@@ -32,7 +32,7 @@ require_once('partials/_analytics.php');
                   <div class="card-body">
                     <div class="row">
                       <div class="col">
-                        <h5 class="card-title text-uppercase text-muted mb-0">OBJETOS DISPONIBLES</h5>
+                        <h5 class="card-title text-uppercase text-muted mb-0">PRODUCTOS DISPONIBLES</h5>
                         <span class="h2 font-weight-bold mb-0"><?php echo $products; ?></span>
                       </div>
                       <div class="col-auto">
--- Restro/customer/pay_order.php
+++ Restro/customer/pay_order.php
@@ -7,67 +7,67 @@ include('config/code-generator.php');
 check_login();
 
 if (isset($_POST['pay'])) {
-  //Prevent Posting Blank Values
-  if (empty($_POST["pay_code"]) || empty($_POST["pay_amt"]) || empty($_POST['pay_method'])) {
-    $err = "Blank Values Not Accepted";
-    //Perform Regex On Payments
-    
-  } else {
+    //Prevent Posting Blank Values
+    if (empty($_POST["pay_code"]) || empty($_POST["pay_amt"]) || empty($_POST['pay_method'])) {
+        $err = "Blank Values Not Accepted";
+        //Perform Regex On Payments
 
-    $pay_Code = $_POST['pay_code'];
+    } else {
 
-      if(strlen($pay_Code) < 10 )
-      {
-        $err = "Payment Code Verification Failed, Please Try Again";
-      }
-      elseif(strlen($pay_Code) > 10)
-      {
-        $err = "Payment Code Verification Failed, Please Try Again";
-      }
-      
-      else
-      {
-          $pay_code = $_POST['pay_code'];
-          $order_code = $_GET['order_code'];
-          $customer_id = $_GET['customer_id'];
-          $pay_amt  = $_POST['pay_amt'];
-          $pay_method = $_POST['pay_method'];
-          $pay_id = $_POST['pay_id'];
+        $pay_Code = $_POST['pay_code'];
 
-          $order_status = $_GET['order_status'];
+        if(strlen($pay_Code) < 10 )
+        {
+            $err = "Payment Code Verification Failed, Please Try Again";
+        }
+        elseif(strlen($pay_Code) > 10)
+        {
+            $err = "Payment Code Verification Failed, Please Try Again";
+        }
 
-          //Insert Captured information to a database table
-          $postQuery = "INSERT INTO rpos_pagos (pago_id, pago_code, pedido_code, cliente_id, pago_monto, pago_metodo) VALUES(?,?,?,?,?,?)";
-          $upQry = "UPDATE rpos_pedidos SET pedido_status =? WHERE pedido_code =?";
+        else
+        {
+            $pay_code = $_POST['pay_code'];
+            $order_code = $_GET['order_code'];
+            $customer_id = $_GET['customer_id'];
+            $pay_amt  = $_POST['pay_amt'];
+            $pay_method = $_POST['pay_method'];
+            $pay_id = $_POST['pay_id'];
 
-          $postStmt = $mysqli->prepare($postQuery);
-          $upStmt = $mysqli->prepare($upQry);
-          //bind paramaters
+            $order_status = $_GET['order_status'];
 
-          $rc = $postStmt->bind_param('ssssss', $pay_id, $pay_code, $order_code, $customer_id, $pay_amt, $pay_method);
-          $rc = $upStmt->bind_param('ss', $order_status, $order_code);
+            //Insert Captured information to a database table
+            $postQuery = "INSERT INTO rpos_pagos (pago_id, pago_code, pedido_code, cliente_id, pago_monto, pago_metodo) VALUES(?,?,?,?,?,?)";
+            $upQry = "UPDATE rpos_pedidos SET pedido_status =? WHERE pedido_code =?";
 
-          $postStmt->execute();
-          $upStmt->execute();
-          //declare a varible which will be passed to alert function
-          if ($upStmt && $postStmt) {
-              $success = "Paid" && header("refresh:1; url=payments_reports.php");
-          } else {
-              $err = "Please Try Again Or Try Later";
-          }
-      }
-  }
+            $postStmt = $mysqli->prepare($postQuery);
+            $upStmt = $mysqli->prepare($upQry);
+            //bind paramaters
+
+            $rc = $postStmt->bind_param('ssssss', $pay_id, $pay_code, $order_code, $customer_id, $pay_amt, $pay_method);
+            $rc = $upStmt->bind_param('ss', $order_status, $order_code);
+
+            $postStmt->execute();
+            $upStmt->execute();
+            //declare a varible which will be passed to alert function
+            if ($upStmt && $postStmt) {
+                $success = "Paid" && header("refresh:1; url=payments_reports.php");
+            } else {
+                $err = "Please Try Again Or Try Later";
+            }
+        }
+    }
 }
 require_once('partials/_head.php');
 ?>
 
 <body>
-  <!-- Sidenav -->
-  <?php
-  require_once('partials/_sidebar.php');
-  ?>
-  <!-- Main content -->
-  <div class="main-content">
+<!-- Sidenav -->
+<?php
+require_once('partials/_sidebar.php');
+?>
+<!-- Main content -->
+<div class="main-content">
     <!-- Top navbar -->
     <?php
     require_once('partials/_topnav.php');
@@ -77,73 +77,73 @@ require_once('partials/_head.php');
     $stmt->execute();
     $res = $stmt->get_result();
     while ($order = $res->fetch_object()) {
-        $total = ($order->prod_precio * $order->prod_cant);
+    $total = ($order->prod_precio * $order->prod_cant);
     ?>
-    
+
     <!-- Header -->
     <div style="background-image: url(../admin/assets/img/theme/restro00.jpg); background-size: cover;" class="header  pb-8 pt-5 pt-md-8">
-    <span class="mask bg-gradient-dark opacity-8"></span>
-      <div class="container-fluid">
-        <div class="header-body">
+        <span class="mask bg-gradient-dark opacity-8"></span>
+        <div class="container-fluid">
+            <div class="header-body">
+            </div>
         </div>
-      </div>
     </div>
     <!-- Page content -->
     <div class="container-fluid mt--8">
-      <!-- Table -->
-      <div class="row">
-        <div class="col">
-          <div class="card shadow">
-            <div class="card-header border-0">
-              <h3>Por favor llena todos los espacios</h3>
+        <!-- Table -->
+        <div class="row">
+            <div class="col">
+                <div class="card shadow">
+                    <div class="card-header border-0">
+                        <h3>Por favor llena todos los espacios</h3>
+                    </div>
+                    <div class="card-body">
+                        <form method="POST"  enctype="multipart/form-data">
+                            <div class="form-row">
+                                <div class="col-md-6">
+                                    <label>ID de pago</label>
+                                    <input type="text" name="pay_id" readonly value="<?php echo $payid;?>" class="form-control">
+                                </div>
+                                <div class="col-md-6">
+                                    <label>Código de pago</label><small class="text-danger"> Escriba el código alfabético de 10 dígitos si el método de pago es en efectivo</small>
+                                    <input type="text" limit="11" name="pay_code" placeholder="<?php echo $mpesaCode; ?>" class="form-control" value="">
+                                </div>
+                            </div>
+                            <hr>
+                            <div class="form-row">
+                                <div class="col-md-6">
+                                    <label>Cantidad (Bs)</label>
+                                    <input type="text" name="pay_amt" readonly value="<?php echo $total;?>" class="form-control">
+                                </div>
+                                <div class="col-md-6">
+                                    <label>Método de pago</label>
+                                    <select class="form-control" name="pay_method">
+                                        <option selected>Dinero</option>
+                                        <option>Paypal</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <br>
+                            <div class="form-row">
+                                <div class="col-md-6">
+                                    <input type="submit" name="pay" value="Pay Order" class="btn btn-success" value="">
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
-            <div class="card-body">
-              <form method="POST"  enctype="multipart/form-data">
-                <div class="form-row">
-                  <div class="col-md-6">
-                    <label>ID de pago</label>
-                    <input type="text" name="pay_id" readonly value="<?php echo $payid;?>" class="form-control">
-                  </div>
-                  <div class="col-md-6">
-                    <label>Código de pago</label><small class="text-danger"> Escriba el código alfabético de 10 dígitos si el método de pago es en efectivo</small>
-                    <input type="text" limit="11" name="pay_code" placeholder="<?php echo $mpesaCode; ?>" class="form-control" value="">
-                  </div>
-                </div>
-                <hr>
-                <div class="form-row">
-                  <div class="col-md-6">
-                    <label>Cantidad (Bs)</label>
-                    <input type="text" name="pay_amt" readonly value="<?php echo $total;?>" class="form-control">
-                  </div>
-                  <div class="col-md-6">
-                    <label>Método de pago</label>
-                    <select class="form-control" name="pay_method">
-                        <option selected>Dinero</option>
-                        <option>Paypal</option>
-                    </select>
-                  </div>
-                </div>
-                <br>
-                <div class="form-row">
-                  <div class="col-md-6">
-                    <input type="submit" name="pay" value="Pay Order" class="btn btn-success" value="">
-                  </div>
-                </div>
-              </form>
-            </div>
-          </div>
         </div>
-      </div>
-      <!-- Footer -->
-      <?php
-      require_once('partials/_footer.php');
-      ?>
+        <!-- Footer -->
+        <?php
+        require_once('partials/_footer.php');
+        ?>
     </div>
-  </div>
-  <!-- Argon Scripts -->
-  <?php
-  require_once('partials/_scripts.php'); }
-  ?>
+</div>
+    <!-- Argon Scripts -->
+<?php
+require_once('partials/_scripts.php'); }
+?>
 </body>
 
 </html>
